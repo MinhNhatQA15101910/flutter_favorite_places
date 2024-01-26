633a73d547bad85b4008c176096344f8cf8dc5c3
Do Nhat <dmnhat.pt@gmail.com>
2024-01-26 21:37:09 +0700

    Adding a "Map" screen.

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,70 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({
+    super.key,
+    this.location = const PlaceLocation(
+      latitude: 37.422,
+      longitude: -122.084,
+      address: '',
+    ),
+    this.isSelecting = true,
+  });
+
+  final PlaceLocation location;
+  final bool isSelecting;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MapScreenState();
+  }
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.isSelecting ? 'Pick your Location' : 'Your Location',
+        ),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.save),
+            ),
+        ],
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(
+            widget.location.latitude,
+            widget.location.longitude,
+          ),
+          initialZoom: 16,
+        ),
+        children: [
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(
+                  widget.location.latitude,
+                  widget.location.longitude,
+                ),
+                child: Icon(
+                  Icons.location_pin,
+                  color: Colors.red.shade700,
+                  size: 60,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
